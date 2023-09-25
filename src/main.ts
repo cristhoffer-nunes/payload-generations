ea38069baf108059607af1e9d448d1dbfc993cbb
cristhoffer-santos <cristhoffer.santos@jbq.global>
2023-09-25 16:59:00 -0300

    feat: add config service to port

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -16,6 +18,6 @@ async function bootstrap() {
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
